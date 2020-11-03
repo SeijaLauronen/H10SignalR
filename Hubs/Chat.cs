8dea0970cf2a54a5e9ac403ee7c81baf32e88f35
Seija Lauronen <seija.lauronen@gmail.com>
2020-11-03 17:37:32 +0200

    Add project files.

---
+++ Hubs/Chat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.SignalR;//SSL
+//https://docs.microsoft.com/en-us/aspnet/core/tutorials/signalr?view=aspnetcore-3.1&tabs=visual-studio
+
+//TODO: Models-kansio? videolla 2 tunnin kohdala
+
+namespace Harjoitus10_SignalRChat.Hubs
+{
+    public class Chat:Hub //SSL Added Hub
+    {
+        //private Chat chatti = new Chat(); //ei tätä nyt tarvita
+        public async Task SendMessage(string user, string message)
+        {
+            var temppi = user; //Kunhan tähän vaan laittoi jotain, niin alkoi toimia, muuten chat vaan vilahti ja sulkeutui!! Missä vika?
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
---
+++ wwwroot/js/omachat.js
@@ -0,0 +1,30 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chat").build();//SSL Tänne se reitin urlin
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    //TODO tänne oman tekstin korostaminen
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " says " + msg;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value; //tämän pitäsi toimia tällaisenaan, vaikka vaihdoin text textarea:ksi
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
